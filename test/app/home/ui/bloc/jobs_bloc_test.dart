788436446c8884f28a649b82fa88691a2864ee74
jhoisz <jvrda@ufpi.edu.br>
2024-07-09 22:29:32 -0300

    fix: fixing tests after changes

--- test/app/home/ui/bloc/jobs_bloc_test.dart
+++ test/app/home/ui/bloc/jobs_bloc_test.dart
@@ -8,13 +8,16 @@ import 'package:mocktail/mocktail.dart';
 
 class MockJobsRepository extends Mock implements IJobsRepository {}
 
+class MockJobModel extends Mock implements JobModel {}
+
 void main() {
   late IJobsBloc bloc;
   late IJobsRepository repository;
-
+  late JobModel mockJob;
   setUp(() {
     repository = MockJobsRepository();
     bloc = JobsBloc(repository: repository);
+    mockJob = MockJobModel();
   });
 
   blocTest(
@@ -48,6 +51,34 @@ void main() {
     expect: () => [
       isA<JobsLoading>(),
       isA<JobsError>(),
+    ],
+  );
+
+  blocTest(
+    'emits [JobsLoading, JobsLoaded, JobsLoading, JobsLoaded] when searchJobs succeeds',
+    build: () {
+      when(() => mockJob.title).thenReturn('test');
+      when(() => mockJob.isActive).thenReturn(true);
+      when(() => repository.getJobs()).thenAnswer((_) async => [mockJob]);
+      return bloc;
+    },
+    act: (bloc) async {
+      await bloc.getJobs();
+      bloc.searchJobs(searchText: 'test');
+    },
+    expect: () => [
+      isA<JobsLoading>(),
+      const TypeMatcher<JobsLoaded>().having(
+        (state) => state.jobs,
+        '',
+        hasLength(1),
+      ),
+      isA<JobsLoading>(),
+      const TypeMatcher<JobsLoaded>().having(
+        (state) => state.jobs,
+        '',
+        hasLength(1),
+      ),
     ],
   );
 }
